eb0eedd1be932587d67751ff7a7e652a3248089e
kitazawa <kitadesign@gmail.com>
2022-08-08 19:41:39 +0900

    nextjs custom server設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ const Index: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
